02a4a9c4beae1edf2c6845e580b9e24e6185e294
Bryan <whompum101@gmail.com>
2018-12-02 12:30:14 -0500

    Rm missing imports

--- app/src/main/java/com/whompum/currencyedittext/MainActivity.java
+++ app/src/main/java/com/whompum/currencyedittext/MainActivity.java
@@ -1,11 +1,7 @@
 package com.whompum.currencyedittext;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
-
-
     }
 
 }
